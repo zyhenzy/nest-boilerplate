f104a4e660d73f731f4102dbdb232d99d572476c
ying.zhang_zhang <ying.zhang_zhang@neusoft.com>
2024-12-03 17:20:25 +0800

    feat: 英雄增删改查

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,24 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { HeroModule } from './hero/hero.module';
+import { Hero } from './hero/entity/hero.entity';
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: 'localhost',
+      port: 3306,
+      username: 'root',
+      password: '123456',
+      database: 'stzb',
+      entities: [Hero],
+      synchronize: true,
+    }),
+    HeroModule
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
