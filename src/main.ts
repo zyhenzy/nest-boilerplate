290261d051c50436a70b960b844b719b02c40312
ying.zhang_zhang <ying.zhang_zhang@neusoft.com>
2024-12-11 16:15:50 +0800

    feat: 全局相应拦截器

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
+import { ResponseInterceptor } from './common/interceptor';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -12,7 +13,9 @@ async function bootstrap() {
     .build();
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('swagger-ui', app, document);
+  app.useGlobalInterceptors(new ResponseInterceptor()); // 启用全局拦截器
+  app.setGlobalPrefix('api'); // 设置全局前缀
 
-  await app.listen(process.env.PORT ?? 3000);
+  await app.listen(process.env.PORT ?? 4869);
 }
 bootstrap();
