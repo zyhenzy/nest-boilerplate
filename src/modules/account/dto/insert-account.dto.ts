501b05d058f957ca05905fb262eef7dc273bd0f7
ying.zhang_zhang <ying.zhang_zhang@neusoft.com>
2024-12-23 14:01:32 +0800

    feat: 新增账号到某条件下

--- src/modules/account/dto/insert-account.dto.ts
+++ src/modules/account/dto/insert-account.dto.ts
@@ -1,13 +1,16 @@
 import { ApiProperty } from '@nestjs/swagger';
+import { IsNotEmpty } from 'class-validator';
 
 export class InsertAccountDto {
   @ApiProperty({
     type: String,
   })
+  @IsNotEmpty()
   game_ordersn: string;
 
   @ApiProperty({
     type: String,
   })
+  @IsNotEmpty()
   conditionId: string;
 }
